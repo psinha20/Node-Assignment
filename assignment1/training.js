cd1a25695c59b136c50c2631caa34416ddb078f9
Prakhar Sinha <psinha@grepruby.com>
2017-08-25 15:44:47 +0530

    updated assignment1

--- assignment1/training.js
+++ assignment1/training.js
@@ -1,67 +1,58 @@
-// Problem 1: Complete the secondLargest function which takes in an array of numbers in input and return the second biggest number in the array. (without using sort)?
+
 function secondLargest(array) {
-  // Write your code here
-  max=Number.MIN_SAFE_INTEGER;
-  max2=Number.MIN_SAFE_INTEGER;
-  
-  for(i=0;i<array.length;i++)
-    {
-      if(array[i]>max) max=array[i];
+  largest=Number.MIN_SAFE_INTEGER;
+  secondLargest=Number.MIN_SAFE_INTEGER;
+  var loop;
+  for(loop=0;loop<array.length;loop++) {
+      if(array[loop]>largest) largest=array[loop];
       
-   }
-  for(i=0;i<array.length;i++)
-    {
-      if(array[i]>max2&&array[i]<max) max2=array[i];
-    }
-  return max2;
-}
-
-// Problem 2: Complete the calculateFrequency function that takes lowercase string as input and returns frequency of all english alphabet. (using only array, no in-built function)
-function calculateFrequency(string) {
-  // Write your code here
-   var freq={};
-    var i;
-  for(i=0;i<string.length;i++)
-    {
-      var x=string.charAt(i);
-      if(x.match(".*[a-z].*")){
-      if(freq[string.charAt(i)] ) {freq[string.charAt(i)]++;}
-      else {freq[string.charAt(i)]=1;}
-      }
-    }
- return freq;
-}
-
-// Problem 3: Complete the flatten function that takes a JS Object, returns a JS Object in flatten format (compressed)
-function flatten(unflatObject) {
-  // Write your code here
-  var ans={};
-  for(var i in unflatObject)
-    {
-        if(typeof (unflatObject[i])=='object')
-         {
-            var flatobject=flatten(unflatObject[i]);
-           for(x in flatobject)
-             {
-                  ans[i+'.'+x]=flatobject[x];
-             }
-         }
-       else
-         ans[i]=unflatObject[i];
-    }
-  return ans;
-}
+    				       			    }
+  for(loop=0;loop<array.length;loop++){
+      if(array[loop]>secondLargest&&array[loop]<largest) secondLargest=array[loop];
+    				      			  }
+  return secondLargest;
+			                  }
 
 
-// Problem 4: Complete the unflatten function that takes a JS Object, returns a JS Object in unflatten format
+function calculateFrequency(string){
+  
+    var freq={};
+    var loop;
+    for(loop=0;loop<string.length;loop++){
+      var stringCharacter=string.charAt(loop);
+      if(stringCharacter.match(".*[a-z].*")){
+      if(freq[string.charAt(loop)] ) {freq[string.charAt(loop)]++;}
+      else {freq[string.charAt(loop)]=1;}
+                                            }
+    				          			 }
+    return freq;
+			            		   }
+
+
+function flatten(unflatObject){
+   var ans={};
+   for(var loop in unflatObject){
+        if(typeof (unflatObject[loop])=='object'){
+            var flatobject=flatten(unflatObject[loop]);
+            for(innerLoop in flatobject){
+                  ans[loop+'.'+innerLoop]=flatobject[innerLoop];
+                                }
+                                                  }
+        else
+            ans[loop]=unflatObject[loop];
+                                 }
+   return ans;
+                              }
+
+
 function unflatten(flatObject) {
-  // Write your code here
+  
   var result = {}
   for (var i in flatObject) {
-    var keys = i.split('.')
+    var keys = i.split('.');
     keys.reduce(function(r, e, j) {
       return r[e] || (r[e] = isNaN(Number(keys[j + 1])) ? (keys.length - 1 == j ? flatObject[i] : {}) : [])
-    }, result)
-  }
+    							  }, result)
+                            }
   return result
-}
+			      			    }
