b3dcf532d9ff433ac59b72bd5b77e81a1c16ae9a
Prakhar Sinha <psinha@grepruby.com>
2017-08-25 15:47:04 +0530

    updated assignment1

--- assignment1/training.js
+++ assignment1/training.js
@@ -47,12 +47,12 @@ function flatten(unflatObject){
 
 function unflatten(flatObject) {
   
-  var result = {}
+  var result = {};
   for (var i in flatObject) {
     var keys = i.split('.');
     keys.reduce(function(r, e, j) {
       return r[e] || (r[e] = isNaN(Number(keys[j + 1])) ? (keys.length - 1 == j ? flatObject[i] : {}) : [])
     							  }, result)
                             }
-  return result
+  return result;
 			      			    }
