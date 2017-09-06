26d68c273139684706a7a5ac3688ee0cb7a61d71
Prakhar Sinha <psinha@grepruby.com>
2017-09-06 12:56:18 +0530

    Suggested changes to promise assignment completed

--- promise.js
+++ promise.js
@@ -1,64 +1,63 @@
 var fs = require('fs');
 
-function step1(){
-	return new Promise(function(resolve,reject){
-    	
-   		fs.readFile('file1.txt', 'utf8', function (err,data) {
-    		if(data) {
-      			resolve(data);
-    		}
-    		else console.log('File 1 not read!');
-  			})
-		})
+function readFile1() {
+    return new Promise(function(resolve, reject) {
+
+        fs.readFile('file1.txt', 'utf8', function(err, data) {
+            if (data) {
+                resolve(data);
+            }
+
+        })
+    })
 }
 
-function step2(param){
-console.log('second parameter'+param);
-return new Promise(function(resolve,reject){
-fs.appendFile('file2.txt', param, function (err) {
-        if(err) {
-          throw err; 
-        }
-        else {
-          console.log('done2');
-          resolve(); 
-        }
-      })
-})
+function writeToFile2(param) {
+    return new Promise(function(resolve, reject) {
+        fs.appendFile('file2.txt', param, function(err) {
+            if (err) {
+                throw err;
+            } else {
+                resolve();
+            }
+        })
+    })
 }
 
- function step3(){
- return new Promise(function(resolve,reject){
-   console.log('reading file 2'); 
-   fs.readFile('file2.txt', 'utf8', function (err,data) {
-    if(data) {
-      console.log('passing'+data);
-      resolve(data);
-    }
-    else console.log('File 2 not read!');
-  })
-})
+function readFile2() {
+    return new Promise(function(resolve, reject) {
+        fs.readFile('file2.txt', 'utf8', function(err, data) {
+            if (data) {
+                resolve(data);
+            }
+
+        })
+    })
 
 }
 
-function step4(paramN){
-console.log('received'+paramN);
-return new Promise(function(resolve,reject){
-fs.appendFile('file3.txt', paramN, function (err) {
-        if(err) {
-          throw err; 
-        }
-        else {
-          console.log('done4');
-          resolve(); 
-        }
-      })
-})
+function writeToFile3(paramN) {
+    return new Promise(function(resolve, reject) {
+        fs.appendFile('file3.txt', paramN, function(err) {
+            if (err) {
+                throw err;
+            } else {
+                resolve();
+            }
+        })
+    })
 }
 
 
-step1()
-.then(step2)
-.then(step3)
-.then(step4)
-.then(function(){console.log('All steps completed');});
+readFile1()
+    .then(writeToFile2).catch(function() {
+        console.log("File 1 not read");
+    }).then(readFile2).catch(function() {
+        console.log("Appending to File2 failed");
+    }).then(writeToFile3).catch(function() {
+        console.log("File 2 not read");
+    }).then(function() {
+        console.log('All functions completed');
+    }).catch(function() {
+        console.log("Appending to File 3 failed");
+    });
