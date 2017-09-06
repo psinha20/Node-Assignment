8c36c90842d790ddc73d1158fc330b481c4efe13
Prakhar Sinha <psinha@grepruby.com>
2017-09-06 12:26:42 +0530

    All requested changes made to to assignment-4

--- node-project/programs/calculateFrequency.js
+++ node-project/programs/calculateFrequency.js
@@ -1,13 +1,15 @@
-exports.calculateFrequency = function (string) {
-  var frequency={};
-  var loop;
-  for(loop=0;loop<string.length;loop++)
-    {
-      var stringIterator=string.charAt(loop);
-      if(stringIterator.match(".*[a-z].*")){
-      if(frequency[string.charAt(loop)] ) {frequency[string.charAt(loop)]++;}
-      else {frequency[string.charAt(loop)]=1;}
-                                            }
+exports.calculateFrequency = function(string) {
+    var frequency = {};
+    var loop;
+    for (loop = 0; loop < string.length; loop++) {
+        var stringIterator = string.charAt(loop);
+        if (stringIterator.match(".*[a-z].*")) {
+            if (frequency[string.charAt(loop)]) {
+                frequency[string.charAt(loop)]++;
+            } else {
+                frequency[string.charAt(loop)] = 1;
+            }
+        }
     }
- return frequency;
+    return frequency;
 }
--- node-project/programs/unflatten.js
+++ node-project/programs/unflatten.js
@@ -1,10 +1,10 @@
-exports.unflatten = function (flatObject) {
-  var result = {};
-  for (var loop in flatObject) {
-    var keys = loop.split('.');
-    keys.reduce(function(r, e, j) {
-      return r[e] || (r[e] = isNaN(Number(keys[j + 1])) ? (keys.length - 1 == j ? flatObject[loop] : {}) : [])
-    }, result)
-  								}	
-  return result
+exports.unflatten = function(flatObject) {
+    var result = {};
+    for (var loop in flatObject) {
+        var keys = loop.split('.');
+        keys.reduce(function(r, e, j) {
+            return r[e] || (r[e] = isNaN(Number(keys[j + 1])) ? (keys.length - 1 == j ? flatObject[loop] : {}) : [])
+        }, result)
+    }
+    return result;
 }
