b577c7ccc947b48a972b7fa42cb2c0c231c87907
Prakhar Sinha <psinha@grepruby.com>
2017-09-06 19:01:10 +0530

    Requested changes made to callback assignment

--- callback.js
+++ callback.js
@@ -1,53 +1,27 @@
-var http = require('http');
-var fse = require('fs-extra');
 var fs = require('fs');
 
-
-var thirdFile;
-const readMultipleFiles = require('read-multiple-files');
-
-function readFile1 (callback) {
- fs.readFile("file1.txt", "UTF8", function(err, data) {
-    if (err) { 
-    	throw err 
-    };
-    firstFile = data.toString();
-    console.log(firstFile);
-    if (callback) callback(firstFile);
+fs.readFile("file1.txt", "UTF8", function(err, data) {
+    fs.appendFile('file2.txt', data, function(err) {
+        fs.readFile("file2.txt", "UTF8", function(err, data) {
+            fs.appendFile('file3.txt', data, function(err) {
+                console.log("End of operations");
+            });
+        });
+    });
 });
+    
+       
+        
+            
+
+            
+
+            
+
+        
+
+    
 
 
 
-}
 
-function appendFile2 (data, callback) {
-	fs.appendFile('file2.txt', data, function (err) {
-  		if (err) throw err;
-  		console.log('Saved!');
-  		if (callback) callback();
-
-});	
-
-}
-
-function readFile2 (callback) {
- fs.readFile("file2.txt", "UTF8", function(err, data) {
-    if (err) { 
-    	throw err 
-    };
-    secondFile = data.toString();
-    if (callback) callback(secondFile);
-});
-
-
-}
-
-function appendFile3 (data) {
-	fs.appendFile('file3.txt', data, function (err) {
-  		if (err) throw err;
-  		console.log('Saved!');
-	});	
-
-}
-
-readFile1(appendFile2(readFile2(appendFile3)));
