d2cb32b7e1f45b1cab1338859f63f87dfc9eb7ea
Prakhar Sinha <psinha@grepruby.com>
2017-09-08 14:19:41 +0530

    Implemented the 2 changes suggested

--- callback.js
+++ callback.js
@@ -1,27 +1,11 @@
 var fs = require('fs');
 
-fs.readFile("file1.txt", "UTF8", function(err, data) {
-    fs.appendFile('file2.txt', data, function(err) {
-        fs.readFile("file2.txt", "UTF8", function(err, data) {
-            fs.appendFile('file3.txt', data, function(err) {
-                console.log("End of operations");
-            });
-        });
+fs.readFile('file1.txt', 'UTF8', function(err, data) {
+  fs.appendFile('file2.txt', data, function(err) {
+    fs.readFile('file2.txt', 'UTF8', function(err, data) {
+      fs.appendFile('file3.txt', data, function(err) {
+        console.log('End of operations');
+      });
     });
+  });
 });
-    
-       
-        
-            
-
-            
-
-            
-
-        
-
-    
-
-
-
-
