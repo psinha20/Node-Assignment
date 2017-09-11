be4ac4fcba2c94f58d0a6a8194ed10fa65c8f624
Prakhar Sinha <psinha@grepruby.com>
2017-09-11 17:44:59 +0530

    requested changes implemented in promise assignment

--- promise.js
+++ promise.js
@@ -1,63 +1,39 @@
 var fs = require('fs');
 
-function readFile1() {
-    return new Promise(function(resolve, reject) {
+function readFile(fileName) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(fileName, 'utf8', function(err, data) {
+      data ? r esolve() : reject();
 
-        fs.readFile('file1.txt', 'utf8', function(err, data) {
-            if (data) {
-                resolve(data);
-            }
-
-        })
     })
+  })
 }
 
 function writeToFile2(param) {
-    return new Promise(function(resolve, reject) {
-        fs.appendFile('file2.txt', param, function(err) {
-            if (err) {
-                throw err;
-            } else {
-                resolve();
-            }
-        })
+  return new Promise(function(resolve, reject) {
+    fs.appendFile('file2.txt', param, function(err) {
+      err ? reject() : resolve();
     })
-}
-
-function readFile2() {
-    return new Promise(function(resolve, reject) {
-        fs.readFile('file2.txt', 'utf8', function(err, data) {
-            if (data) {
-                resolve(data);
-            }
-
-        })
-    })
-
+  })
 }
 
 function writeToFile3(paramN) {
-    return new Promise(function(resolve, reject) {
-        fs.appendFile('file3.txt', paramN, function(err) {
-            if (err) {
-                throw err;
-            } else {
-                resolve();
-            }
-        })
+  return new Promise(function(resolve, reject) {
+    fs.appendFile('file3.txt', paramN, function(err) {
+      err ? reject() : resolve();
     })
+  })
 }
 
-
-readFile1()
-    .then(writeToFile2).catch(function() {
-        console.log("File 1 not read");
-    }).then(readFile2).catch(function() {
-        console.log("Appending to File2 failed");
-    }).then(writeToFile3).catch(function() {
-        console.log("File 2 not read");
-    }).then(function() {
-        console.log('All functions completed');
-    }).catch(function() {
-        console.log("Appending to File 3 failed");
-    });
+readFile('file1.txt')
+  .then(writeToFile2).catch(function() {
+    console.log("File 1 not read");
+  }).then(readFile('file2.txt')).catch(function() {
+    console.log("Appending to File2 failed");
+  }).then(writeToFile3).catch(function() {
+    console.log("File 2 not read");
+  }).then(function() {
+    console.log('All functions completed');
+  }).catch(function() {
+    console.log("Appending to File 3 failed");
+  });
