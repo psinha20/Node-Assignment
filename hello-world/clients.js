ad314862cb56ebd77738dfb3ca2e276a4e870cf9
Prakhar Sinha <psinha@grepruby.com>
2017-09-11 17:51:14 +0530

    suggested changes made to campaign monitor assignment

--- hello-world/clients.js
+++ hello-world/clients.js
@@ -4,74 +4,72 @@ var client = new Client();
 var router = express.Router();
 var rp = require('request-promise');
 var request = require('request');
-
 module.exports = router;
 
 router.get('/', function(req, res, next) {
-    request.get('https://api.createsend.com/api/v3.1/clients.json', {
-        'auth': {
-            'user': '9a86a35217185b745506196eb27709a0',
-            'pass': 'x'
-        }
-    }, function(error, response, body) {
-        if (!error) {
-            res.send(body)
-        } else console.log(error);
-    })
+  request.get('https://api.createsend.com/api/v3.1/clients.json', {
+    'auth': {
+      'user': '9a86a35217185b745506196eb27709a0',
+      'pass': 'x'
+    }
+  }, function(error, response, body) {
+    if (!error) {
+      res.send(body)
+    } else console.log(error);
+  })
 
 });
 
-
 router.get('/:id/lists.json', function(req, res, next) {
-    request.get("https://api.createsend.com/api/v3.1/clients/" + req.params.id + "/lists.json", {
-        'auth': {
-            'user': '9a86a35217185b745506196eb27709a0',
-            'pass': 'x'
-        }
-    }, function(error, response, body) {
-        if (!error) {
-            res.send(body)
-        } else console.log(error);
-    })
+  request.get('https://api.createsend.com/api/v3.1/clients/' + req.params.id + '/lists.json', {
+    'auth': {
+      'user': '9a86a35217185b745506196eb27709a0',
+      'pass': 'x'
+    }
+  }, function(error, response, body) {
+    if (!error) {
+      res.send(body)
+    } else console.log(error);
+  })
 
 });
 
 router.get('/:id', function(req, res) {
-    var clientId = req.params.id;
-    request.get("https://api.createsend.com/api/v3.1/clients/" + clientId + ".json", {
-        'auth': {
-            'user': '9a86a35217185b745506196eb27709a0',
-            'pass': 'x'
-        }
-    }, function(error, response, body) {
-        if (!error) {
-            res.send(body)
-        } else console.log(error);
-    })
+  var clientId = req.params.id;
+  request.get('https://api.createsend.com/api/v3.1/clients/' + clientId + '.json', {
+    'auth': {
+      'user': '9a86a35217185b745506196eb27709a0',
+      'pass': 'x'
+    }
+  }, function(error, response, body) {
+    if (!error) {
+      res.send(body)
+    } else console.log(error);
+  })
 });
 
 router.delete('/:id', function(req, res) {
-    var clientId = req.params.id;
-    request.delete("https://api.createsend.com/api/v3.1/clients/" + clientId + ".json", {
-        'auth': {
-            'user': '9a86a35217185b745506196eb27709a0',
-            'pass': 'x'
-        }
-    }, function(error, response, body) {
-        if (!error) {
-            res.send(body)
-        } else console.log(error);
-    })
+  var clientId = req.params.id;
+  request.delete('https://api.createsend.com/api/v3.1/clients/' + clientId + '.json', {
+    'auth': {
+      'user': '9a86a35217185b745506196eb27709a0',
+      'pass': 'x'
+    }
+  }, function(error, response, body) {
+    if (!error) {
+      res.send(body)
+    } else console.log(error);
+  })
 });
 
-router.post("/", function(req, res) {
-    var remote = request('https://api.createsend.com/api/v3.1/clients.json', {
-        'auth': {
-            'user': '9a86a35217185b745506196eb27709a0',
-            'pass': 'x'
-        }
-    });
+router.post('/', function(req, res) {
+  var remote = request('https://api.createsend.com/api/v3.1/clients.json', {
+    'auth': {
+      'user': '9a86a35217185b745506196eb27709a0',
+      'pass': 'x'
+    }
+  });
 
-    req.pipe(remote);
-    remote.pipe(res);
+  req.pipe(remote);
+  remote.pipe(res);
 });
--- hello-world/index.js
+++ hello-world/index.js
@@ -2,18 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
-var cookieParser = require('cookie-parser');
 var app = express();
+var clients = require('./clients.js');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
-
-
-var clients = require('./clients.js');
-
-
 app.use('/clients', clients);
-
 app.listen(8000);
