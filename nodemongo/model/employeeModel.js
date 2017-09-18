dd28823a26f93af5e7d6b63669d0ee70333d504b
Prakhar Sinha <psinha@grepruby.com>
2017-09-18 20:27:34 +0530

    requested chanes implemented on 18 september

--- nodemongo/model/employeeModel.js
+++ nodemongo/model/employeeModel.js
@@ -1,109 +1,130 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var multer = require('multer'); 
-var upload = multer(); 
 var app = require('express')();
-app.use(bodyParser.json()); 
-
-app.use(bodyParser.urlencoded({ extended: true })); 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 var router = express.Router();
-
 var request = require('request');
 var http = require('http');
-
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test');
 var db = mongoose.connection;
 var Schema = mongoose.Schema;
-
 var employeeSchema = new Schema({
   e_id: Number,
   name: String,
   manager_id: Number
-  
+
 });
 var Employee = mongoose.model('Employee', employeeSchema);
 var projectImport = require('./projectModel.js');
 var Project = projectImport.Project;
 
 
- exports.showAllEmployees=function()
-{
-Employee.find({}, function(err, users) {
-  if (err) throw err;
-  console.log(users);
-});
+exports.showAllEmployees = function() {
+  Employee.find({}, function(err, users) {
+    if (err) throw err;
+    console.log(users);
+  });
 }
 
-exports.getEmployeeByName=function(nameSearch)
-{
-Employee.findOne({name:nameSearch},function (err,doc){
-console.log(doc);
-});
+exports.getEmployeeByName = function(nameSearch) {
+  Employee.findOne({
+    name: nameSearch
+  }, function(err, doc) {
+    console.log(doc);
+  });
 }
 
-exports.getEmployeeById=function(idSearch)
-{
-Employee.findOne({e_id:idSearch},function (err,doc){
-console.log(doc);
-});
+exports.getEmployeeById = function(idSearch) {
+  Employee.findOne({
+    e_id: idSearch
+  }, function(err, doc) {
+    console.log(doc);
+  });
 }
 
-exports.getEmployeesByManagees=function(idSearch)
-{
-	Employee.findOne({manager_id:idSearch},function (err,doc){
-
-console.log(doc);
-});
+exports.getEmployeesByManagees = function(idSearch) {
+  Employee.findOne({
+    manager_id: idSearch
+  }, function(err, doc) {
+    console.log(doc);
+  });
 }
 
-exports.insertNewEmployee=function(idNew,nameNew,managerNew)
-{
-var newEmployee=Employee();
-newEmployee.e_id=idNew;
-newEmployee.name=nameNew;
-newEmployee.manager_id=managerNew;
-
-newEmployee.save(function(err){
-if (err) throw err;
-console.log("User Saved");
-})
+exports.insertNewEmployee = function(idNew, nameNew, managerNew) {
+  var newEmployee = Employee();
+  newEmployee.e_id = idNew;
+  newEmployee.name = nameNew;
+  newEmployee.manager_id = managerNew;
+  newEmployee.save(function(err) {
+    if (err) throw err;
+    console.log("User Saved");
+  })
 }
 
- exports.deleteEmployee=function(id)
-{
-Employee.findOneAndRemove({ e_id:id }, function(err) {
-  if (err) throw err;
-console.log('employee deleted!');
-});
-Employee.update({ e_id: id}, { $set: { manager_id: null }}, function(err){
-if(err) throw err;
-console.log('updated manager');
-});
-Project.update({ managed_by: id }, { $set: { managed_by: null }}, function(err){
-if(err) throw err;
-console.log('managed by updated');
-});
-Project.update({},{$pull: {employees: id}}, {multi: true},function(err)
-{
-console.log("given"+id);
-});
+exports.deleteEmployee = function(id) {
+  Employee.findOneAndRemove({
+    e_id: id
+  }, function(err) {
+    if (err) throw err;
+    console.log('employee deleted!');
+  });
+  Employee.update({
+    e_id: id
+  }, {
+    $set: {
+      manager_id: null
+    }
+  }, function(err) {
+    if (err) throw err;
+    console.log('updated manager');
+  });
+  Project.update({
+    managed_by: id
+  }, {
+    $set: {
+      managed_by: null
+    }
+  }, function(err) {
+    if (err) throw err;
+    console.log('managed by updated');
+  });
+  Project.update({}, {
+    $pull: {
+      employees: id
+    }
+  }, {
+    multi: true
+  }, function(err) {
+    console.log("given" + id);
+  });
 }
 
-exports.updateEmployeeName=function(id,nameNew)
-{
-Employee.update({ e_id: id}, { $set: { name: nameNew }}, function(err){
-if(err) throw err;
-console.log('updated');
-});
-
+exports.updateEmployeeName = function(id, nameNew) {
+  Employee.update({
+    e_id: id
+  }, {
+    $set: {
+      name: nameNew
+    }
+  }, function(err) {
+    if (err) throw err;
+    console.log('updated');
+  });
 }
 
-exports.updateEmployeeManager=function(id,managerId)
-{
-Employee.update({ e_id: id}, { $set: { manager_id: managerId }}, function(err){
-if(err) throw err;
-console.log('updated manager');
-});
+exports.updateEmployeeManager = function(id, managerId) {
+  Employee.update({
+    e_id: id
+  }, {
+    $set: {
+      manager_id: managerId
+    }
+  }, function(err) {
+    if (err) throw err;
+    console.log('updated manager');
+  });
 }
--- nodemongo/model/projectModel.js
+++ nodemongo/model/projectModel.js
@@ -6,91 +6,104 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test');
 var db = mongoose.connection;
 var Schema = mongoose.Schema;
-
 var projectSchema = new Schema({
   p_id: Number,
   title: String,
   employees: Array,
   managed_by: Number
-  
+
 });
 var Project = mongoose.model('Project', projectSchema);
-exports.Project=Project;
-exports.getAllProjects=function()
-{
-	Project.find({}, function(err, users) {
-  if (err) throw err;
-  console.log(users);
-});
+exports.Project = Project;
+
+exports.getAllProjects = function() {
+  Project.find({}, function(err, users) {
+    if (err) throw err;
+    console.log(users);
+  });
 
 }
 
-exports.getProjectByName=function(nameSearch)
-{
-Project.findOne({title:nameSearch},function (err,doc){
-
-console.log(doc);
-});
+exports.getProjectByName = function(nameSearch) {
+  Project.findOne({
+    title: nameSearch
+  }, function(err, doc) {
+    console.log(doc);
+  });
 }
 
-exports.getProjectById=function(idSearch)
-{
-Project.findOne({p_id:idSearch},function (err,doc){
-
-console.log(doc);
-});
+exports.getProjectById = function(idSearch) {
+  Project.findOne({
+    p_id: idSearch
+  }, function(err, doc) {
+    console.log(doc);
+  });
 }
 
-exports.getProjectsByManager=function(id)
-{
-	Project.findOne({managed_by:id},function (err,doc){
-
-console.log(doc);
-});
+exports.getProjectsByManager = function(id) {
+  Project.findOne({
+    managed_by: id
+  }, function(err, doc) {
+    console.log(doc);
+  });
 }
 
-exports.addProject=function(newId,newTitle,NewEmployees,newManager)
-{
-var newProject = Project();
-newProject.p_id=newId;
-newProject.title=newTitle;
-newProject.employees=NewEmployees;
-newProject.managed_by=newManager;	
-newProject.save(function(err){
-if (err) throw err;
-console.log("User Saved");
-})
+exports.addProject = function(newId, newTitle, NewEmployees, newManager) {
+  var newProject = Project();
+  newProject.p_id = newId;
+  newProject.title = newTitle;
+  newProject.employees = NewEmployees;
+  newProject.managed_by = newManager;
+  newProject.save(function(err) {
+    if (err) throw err;
+    console.log("User Saved");
+  })
 }
 
-exports.deleteProjectById=function(idSearch)
-{
-	Project.findOneAndRemove({ p_id:idSearch }, function(err) {
-  if (err) throw err;
-
-  console.log('project deleted!');
-});
+exports.deleteProjectById = function(idSearch) {
+  Project.findOneAndRemove({
+    p_id: idSearch
+  }, function(err) {
+    if (err) throw err;
+    console.log('project deleted!');
+  });
 }
 
-exports.updateProjectTitle=function(id,titleNew)
-{
-	Project.update({ p_id: id }, { $set: { title: titleNew }}, function(err){
-if(err) throw err;
-console.log('updated title');
-});
+exports.updateProjectTitle = function(id, titleNew) {
+  Project.update({
+    p_id: id
+  }, {
+    $set: {
+      title: titleNew
+    }
+  }, function(err) {
+    if (err) throw err;
+    console.log('updated title');
+  });
 }
 
-exports.updateProjectManager=function(id,managerNew)
-{
-Project.update({ p_id: id }, { $set: { managed_by: managerNew }}, function(err){
-if(err) throw err;
-console.log('updated manager');
-});
+exports.updateProjectManager = function(id, managerNew) {
+  Project.update({
+    p_id: id
+  }, {
+    $set: {
+      managed_by: managerNew
+    }
+  }, function(err) {
+    if (err) throw err;
+    console.log('updated manager');
+  });
 }
 
-exports.addEmployeeToProject=function (id,newEmployee)
-{
-Project.update({ p_id: id }, { $push: { employees: newEmployee }}, function(err){
-if(err) throw err;
-console.log('updated employees');
-});
+exports.addEmployeeToProject = function(id, newEmployee) {
+  Project.update({
+    p_id: id
+  }, {
+    $push: {
+      employees: newEmployee
+    }
+  }, function(err) {
+    if (err) throw err;
+    console.log('updated employees');
+  });
 }
